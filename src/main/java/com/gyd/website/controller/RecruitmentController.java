c838cfff094258b5bae93fe13751238bfe021d31
lianhong <lianhong@mama.cn>
2019-10-18 13:21:26 +0800

    master 释放静态资源

---
+++ src/main/java/com/gyd/website/controller/RecruitmentController.java
@@ -0,0 +1,34 @@
+package com.gyd.website.controller;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.gyd.website.model.GydMenu;
+import com.gyd.website.repository.IndexDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+/**
+ * @author lianhong
+ * @description
+ * @date 2019/10/17 0017上午 9:36
+ */
+@RestController
+@Slf4j
+public class RecruitmentController {
+    @Autowired
+    private IndexDao indexDao;
+    @GetMapping("/recruit/info/{menuId}")
+    public ModelAndView recruitmentInfo(HttpServletRequest request,@PathVariable Long menuId) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("page/recruitment/recruitment");
+        GydMenu incruitMent = indexDao.findById(menuId).get();
+        mv.addObject("incruitMent",incruitMent);
+        return mv;
+    }
+}
